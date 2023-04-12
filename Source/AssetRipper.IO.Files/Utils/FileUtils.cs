eb5e87c7f13a9d6e02d238c9c9a43d779245e6ff
eelgame <eelgame@qq.com>
2023-04-12 16:27:15 +0800

    bugfix

--- Source/AssetRipper.IO.Files/Utils/FileUtils.cs
+++ Source/AssetRipper.IO.Files/Utils/FileUtils.cs
@@ -65,7 +65,7 @@ namespace AssetRipper.IO.Files.Utils
 
 			for (int counter = initial; counter < int.MaxValue; counter++)
 			{
-				string proposedName = $"{name}_{counter}{ext}";
+				string proposedName = $"{name}_dup_{counter}{ext}";
 				if (!File.Exists(Path.Combine(dirPath, proposedName)))
 				{
 					UniqueNamesByInitialPath[key] = counter;
